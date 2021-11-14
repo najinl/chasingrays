a528f092edd2aedfdb77153462caae08996067d2
Nadia Naji <nadialnaji@gmail.com>
2021-11-14 11:06:26 -0700

    Relocate temperature div

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -12,8 +12,8 @@ const WeatherCard = ({ filteredWeather }) => {
             <div className="date">
               <div className="day-of-week">{day.name.substring(0,3)}</div>
               <div className="day">{day.startTime.substring(8,10)}</div>
+              <div className="temperature">{`${day.temperature}°${day.temperatureUnit}`}</div>
             </div>
-            <div className="temperature">{`${day.temperature}°${day.temperatureUnit}`}</div>
             <img className="weather-img" src={day.icon} alt="representative weather image"/>
             <div className="short-forecast">{day.shortForecast}</div>
         </div>
